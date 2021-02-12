538f9445349d5b3483e7998fcbbd1b39e9ee7bef
dfitch18 <58782044+dfitch18@users.noreply.github.com>
2021-02-12 08:12:48 -0600

    Changed listening port in loginAPI.js

--- src/loginAPI.js
+++ src/loginAPI.js
@@ -1,6 +1,6 @@
 let validateCredentials = function (request) {
     let [user, password] = request;
-    fetch("https://localhost:3000/validateCredentials?user=" + user + "&password=" + password)
+    fetch("https://localhost:8080/validateCredentials?user=" + user + "&password=" + password)
         .then(handleCredentialsSuccess)
         .catch(handleError);
 };
@@ -32,7 +32,7 @@ let handleError = function (textStatus, errorThrown) {
 
 let handleQuestions = function (request) {
     let answers = request;
-    fetch("https://localhost:3000/questions?answers=" + answers[0] + "," + answers[1] + "," + answers[2])
+    fetch("https://localhost:8080/questions?answers=" + answers[0] + "," + answers[1] + "," + answers[2])
         .then(handleQuestionsSuccess)
         .catch(handleError)
 
